e69d5cf77167ee9bd85d19045b746f274a6d3fea
Anamika109 <anamika.deepak19@gmail.com>
2026-09-17 20:02:20 -0400

    redoing it ahope it saves

--- script.js
+++ script.js
@@ -2,24 +2,40 @@ let score = 0
 let timer = 0
 let timeLeft = 90
 let playing = false
+const points = document.getElementById("point")
+const times = document.getElementById("time")
+const starting = document.getElementById("start")
+const scoring = document.getElementById("emoji")
+
+starting.addEventListener("click", play)
+scoring.addEventListener("click", add)
 function play() {
     playing = true
     score = 0
-    document.getElementById("point").innerHTML = score
+    points.innerHTML = score
     timeLeft = 90
-    document.getElementById("time").innerHTML = timeLeft
+    times.innerHTML = timeLeft
     timer = setInterval(upTime, 1000) 
 }
 
 function upTime() {
-    timer = timer + 1
     timeLeft = timeLeft - 1
-    document.getElementById("time").innerHTML = timeLeft
-    if ( timeLeft = 0 ) {
+    times.innerHTML = timeLeft
+    if ( timeLeft === 0 ) {
         stopGame()
     }
 }
 
 function stopGame() {
+    clearInterval(timer);
+    playing = false
+}
+
+function add() {
+
+    if ( playing === true ) {
+        score ++
+        points.innerHTML = score
+    }
 
 }

11c59e515fee80c43983625cc9185cc307bcfd17
Anamika109 <anamika.deepak19@gmail.com>
2026-09-18 18:45:17 -0400

    finisheded

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 let score = 0
 let timer = 0
-let timeLeft = 90
+let timeLeft = 20
 let playing = false
 const points = document.getElementById("point")
 const times = document.getElementById("time")
 const starting = document.getElementById("start")
 const scoring = document.getElementById("emoji")
+const outcome = document.getElementById("result")
 
 starting.addEventListener("click", play)
 scoring.addEventListener("click", add)
 function play() {
+    starting.disabled=true
     playing = true
     score = 0
     points.innerHTML = score
-    timeLeft = 90
+    timeLeft = 20
     times.innerHTML = timeLeft
     timer = setInterval(upTime, 1000) 
+    repeat = setInterval(move, 500)
 }
 
 function upTime() {
@@ -23,12 +26,16 @@ function upTime() {
     times.innerHTML = timeLeft
     if ( timeLeft === 0 ) {
         stopGame()
-    }
+    } 
 }
 
 function stopGame() {
+    starting.disabled=false
     clearInterval(timer);
+    clearInterval(repeat);
     playing = false
+    outcome.innerHTML = "You have " + score + " points!"
+    
 }
 
 function add() {
@@ -38,4 +45,11 @@ function add() {
         points.innerHTML = score
     }
 
+}
+
+function move() {
+    let x = Math.random()*444
+    let y = Math.random()*240
+    scoring.style.left = x + "px";
+    scoring.style.top = y + "px";
 }
